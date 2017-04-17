f90003280075876058a7dbe151af9e8da87c3212
Nachex <elnano@gmail.com>
2017-04-18 00:22:26 +0200

    Add both program tasks for chapter 49

---
+++ program1.js
@@ -0,0 +1,7 @@
+var a = 5;
+var b = 10;
+console.log('a value: ' + a + ' - b value: '+ b);
+var temp = a;
+a = b;
+b = temp;
+console.log('a value: ' + a + ' - b value: '+ b);
---
+++ program2.js
@@ -0,0 +1,6 @@
+var a = 5;
+var b = 10;
+
+leftPart = a*a - a*b + a*b - b*b;
+rightPart = a*a - b*b;
+console.log(leftPart == rightPart);
